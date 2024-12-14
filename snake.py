bea37970e68ed49e8d5d8c824bc41c1651b65ee5
Tom Howes <thomas.howes01@gmail.com>
2024-12-14 15:53:28 -0500

    added score mechanism and graphics

--- snake.py
+++ snake.py
@@ -121,6 +121,7 @@ class Main:
         self.draw_grass()
         self.snake.draw_snake()
         self.fruit.draw_fruit()
+        self.draw_score()
 
     def check_collision(self):
         if self.fruit.pos == self.snake.body[0]:
@@ -153,6 +154,17 @@ class Main:
                         grass_rect = pygame.Rect(col * cell_size, row * cell_size, cell_size, cell_size)
                         pygame.draw.rect(screen, grass_colour, grass_rect)
           
+    def draw_score(self):
+        score_text = str(len(self.snake.body) - 3)
+        score_surface = game_font.render(score_text, True, (56, 74, 12))
+        score_x = int(cell_size * cell_number - 60)
+        score_y = int(cell_size * cell_number - 40)
+        score_rect = score_surface.get_rect(center = (score_x, score_y))
+        apple_rect = score_apple.get_rect(midright = (score_rect.left - 10, score_rect.centery))
+        bg_rect = pygame.Rect(apple_rect.left, apple_rect.top, apple_rect.width + score_rect.width + 20, apple_rect.height)
+        screen.blit(score_surface, score_rect)
+        screen.blit(score_apple, apple_rect)
+        pygame.draw.rect(screen, (56, 74, 12), bg_rect, 2)
 
 pygame.init()
 cell_size = 40
@@ -161,6 +173,9 @@ cell_number = 20
 screen = pygame.display.set_mode((cell_number * cell_size, cell_number * cell_size))
 clock = pygame.time.Clock()
 apple = pygame.image.load("graphics/apple.png").convert_alpha()
+score_apple = pygame.image.load("graphics/apple.png")
+score_apple.set_alpha(160)
+game_font = pygame.font.Font("font/Eating Pasta.ttf", 25)
 
 SCREEN_UPDATE = pygame.USEREVENT
 
